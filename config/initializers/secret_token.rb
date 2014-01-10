5e20bf9aa675feabf91ed3bdedd9de6ccf7952b8
JoseOrtiz <pepe.job@gmail.com>
2014-01-09 21:03:49 -0300

    Agregado secret key

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VidasOnline::Application.config.secret_token = 'cefc0cf240804145b4d67e3d8ccef861d679e735440c004453b32a4ecb820276ae55ef92c0604f8810afed0ee97659bba313e325acf95d35ed3c291b98a7819a'
+VidasOnline::Application.config.secret_key_base= 'ac15fc81312dfec1d4dbae940d407e7b2d4f2103976f3cca707c76fd8e874f634a81b350d48b735575c0088a123df619b1ebbc5cf0cefc2ab4ca28b96de1211b'
